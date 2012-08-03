3cdf0361be22dc68ac028cc6941e393a7e8f6a12
Nick <nick.dietel@gmail.com>
2012-08-03 22:16:57 +0200

    adding sandbox specific files

---
+++ ajax.php
@@ -0,0 +1,11 @@
+<?php
+use dsx\PAX\Response;
+
+require_once 'lib/dsx/PAX/Responder.php';
+require_once 'lib/dsx/PAX/Response.php';
+require_once 'lib/dsx/PAX/Task.php';
+$oPAX=new Response();
+// $oPAX->html('body', 'test');
+// $oPAX->prepend('body', 'test2ä<div>ötest</div>');
+$oPAX->console('I am a test');
+$oPAX->answer();
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>PAX - Demo</title>
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.js"></script>
+<script type="text/javascript" src="lib/dsx/PAX/jquery.PAX.js"></script>
+<script type="text/javascript" src="lib/dsx/PAX/jquery.PAX.actions.js"></script>
+<script type="text/javascript">
+<!--
+	function send(){
+		$.ajax({
+				url: "ajax.php",
+			});
+	}
+//-->
+</script>
+
+</head>
+<body>
+	<a href="#" onclick="send();return false;">Click</a>
+	<div id="target"></div>
+</body>
+</html>
