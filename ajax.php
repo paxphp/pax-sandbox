96082a1a2a63bb2f1377e567cab0db60226c69aa
Nick <nick.dietel@gmail.com>
2012-08-15 01:09:15 +0200

    adding more pages and features to the sandbox for demonstration purposes

--- ajax.php
+++ ajax.php
@@ -1,12 +1,23 @@
 <?php
 require_once 'bootstrap.php';
 use dsx\PAX\Response;
-$oPAX=new Response();
-$oTwig = new Twig_Environment(new Twig_Loader_Filesystem(__DIR__."/templates"));
+$oPAX	= new Response();
+$oTwig	= new Twig_Environment(new Twig_Loader_Filesystem(__DIR__."/templates"));
 
-switch($_GET['do']){
+
+switch(isset($_GET['do'])?$_GET['do']:null){
 	case 'initPage':
 		$oPAX->html('body',$oTwig->render('body.html'));
+		break;
+	case 'about':
+		$oPAX->html('#content',$oTwig->render('pages/about.html'));
+		break;
+	case 'docs':
+		$oPAX->html('#content',$oTwig->render('pages/docs.html'));
+		break;
+	case 'examples':
+		$oPAX->html('#content',$oTwig->render('pages/examples.html'));
+		$oPAX->script('$("pre.php").snippet("php",{style:"golden",showNum:false,menu:false});');
 		break;
 	default:
 		$oPAX->console(var_export($_REQUEST, true));
--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>PAX - Demo</title>
+<title>PAX - phP Ajax eXtension</title>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<link href="http://www.steamdev.com/snippet/css/jquery.snippet.min.css" rel="stylesheet" type="text/css" />
+
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.js"></script>
 <script type="text/javascript" src="lib/dsx/PAX/jquery.PAX.js"></script>
 <script type="text/javascript" src="lib/dsx/PAX/jquery.PAX.actions.js"></script>
+<script type="text/javascript" src="http://www.steamdev.com/snippet/js/jquery.snippet.min.js"></script>
 <script type="text/javascript">
 <!--
 	$.ajaxSetup({
@@ -15,9 +19,6 @@
 	});
 //-->
 </script>
-
 </head>
-<body>
-	
-</body>
+<body></body>
 </html>
