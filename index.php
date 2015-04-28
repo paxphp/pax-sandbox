502196c8cd90b9c647cd4d408b8b2eb82f85ebd3
Nick <ndietel@e-grafixx.net>
2015-04-28 17:50:12 +0200

    updated jquery from 1.7 to 2.1

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <link href="css/style.css" rel="stylesheet" type="text/css" />
 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.5/styles/zenburn.min.css">
 
-<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.js"></script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.5/highlight.min.js"></script>
 <script type="text/javascript" src="vendor/pax/pax/jquery.pax.js"></script>
 <script type="text/javascript" src="js/toc.min.js"></script>
